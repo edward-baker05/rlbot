87df3047502089a28e18bf5e4426667b982a0770
edward-baker05 <ed9baker@outlook.com>
2026-08-24 18:07:58 +0100

    Added a directional ball velocity delta reward

--- bot/src/env/Rewards.h
+++ bot/src/env/Rewards.h
@@ -3,11 +3,14 @@
 #include "../Config.h"
 
 #include "Rewards/ZeroSumReward.h"
+#include <RLGymCPP/Math.h>
 #include <RLGymCPP/Rewards/Reward.h>
 
 #include <functional>
 #include <string>
 #include <vector>
+
+using namespace RLGC;
 
 namespace Dash {
 
@@ -15,6 +18,38 @@ struct RewardSpec {
 	std::string name;
 	float weight;
 	std::function<RLGC::Reward *()> make;
+};
+
+class DirectionalTouchReward : public Reward {
+  public:
+	constexpr static float MAX_REWARDED_BALL_SPEED = RLGC::Math::KPHToVel(110);
+
+	virtual float GetReward(const Player &player, const GameState &state,
+							bool isFinal) {
+		if (!state.prev)
+			return 0;
+
+		if (player.ballTouchedStep) {
+			float prevSpeedFrac = RS_MIN(1, state.prev->ball.vel.Length() /
+												MAX_REWARDED_BALL_SPEED);
+			float curSpeedFrac =
+				RS_MIN(1, state.ball.vel.Length() / MAX_REWARDED_BALL_SPEED);
+
+			if (curSpeedFrac > prevSpeedFrac) {
+				bool targetOrangeGoal = player.team == Team::BLUE;
+
+				Vec targetPos = targetOrangeGoal
+									? CommonValues::ORANGE_GOAL_BACK
+									: CommonValues::BLUE_GOAL_BACK;
+
+				Vec ballDirToGoal = (targetPos - state.ball.pos).Normalized();
+				return ballDirToGoal.Dot(
+					(state.ball.vel - state.prev->ball.vel) /
+					MAX_REWARDED_BALL_SPEED);
+			}
+		}
+		return 0;
+	}
 };
 
 std::vector<RewardSpec> GeneralRewardSpecs(const TrainConfig &cfg);
