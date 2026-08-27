6365a47389a75e8e55f2bce7bd14895feabc53e3
edward-baker05 <ed9baker@outlook.com>
2026-08-27 22:07:42 +0100

    Tweaked boost reward to punish retaining boost on kickoffs instead of encouraging

--- bot/src/env/CommunityRewards.h
+++ bot/src/env/CommunityRewards.h
@@ -577,6 +577,8 @@ class SaveBoostReward : public Reward {
   public:
 	virtual float GetReward(const Player &player, const GameState &state,
 							bool isFinal) override {
+		if (state.ball.pos.x == 0 && state.ball.pos.y == 0)
+			return -0.01f * player.boost;
 		return (player.boost > 20 ? player.boost : 20 - player.boost) * 0.01;
 	}
 };
